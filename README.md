d6376e23460c437e83ac41a6cc8b6ac5bbc6c691
Jonathan <ondesmartenot@riseup.net>
2022-07-10 19:27:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+A simple tool written in Haskell to remove trailing whitespaces from text.
+
+It can be invoked via the command line with
+`./remove-trailing-whitespace < inFile > outFile`
+or in Vim by first selecting text in visual mode, and then typing
+`:!./remove-trailing-whitespace`
---
+++ remove-trailing-whitespace.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Text (Text)
+import Data.Text (isSuffixOf)
+import Data.Text (dropEnd)
+
+import qualified Data.Text
+import qualified Data.Text.IO
+
+deleteLastChar :: Text -> Text
+deleteLastChar "" = error "Empty String!"
+deleteLastChar n = dropEnd 1 n
+
+deleteTrailingSpaces :: Text -> Text
+deleteTrailingSpaces line =
+  if " " `isSuffixOf` line then
+    deleteTrailingSpaces (deleteLastChar line)
+  else
+    line
+
+adjustText :: Text -> Text
+adjustText oldText = newText
+  where
+    oldLines = Data.Text.lines oldText
+    newLines = map deleteTrailingSpaces oldLines
+    newText = Data.Text.unlines newLines
+
+
+main :: IO ()
+main = Data.Text.IO.interact adjustText
